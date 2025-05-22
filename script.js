8aaf03f62d7f82b38be952224d421b8e52566ef0
dnoliveira7 <dennis.ooliveira31@gmail.com>
2025-05-22 20:08:35 -0300

    Add files via upload
    Atualizei imagem

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function fazerPedido(produto, preco) {
-    const numero = '5598984832993'; // Seu número de WhatsApp já configurado
-    const mensagem = `Olá! Quero fazer um pedido:\n🍫 Produto: ${produto}\n💰 Preço: R$ ${preco},00`;
+    const numero = '5598984832993'; // Seu número WhatsApp (com DDI + DDD)
+    const mensagem = `Olá! Quero fazer um pedido:\n🍫 Produto: ${produto}\n💰 Preço: R$ ${preco.toFixed(2)}`;
     const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
     window.open(url, '_blank');
 }
